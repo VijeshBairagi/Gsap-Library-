42469363071d1d96c24fbc7851f1c20d107c604c
Vijju Bairagi <vijeshbairagi45@gmail.com>
2025-10-27 10:27:30 +0530

    Svg Animation Create Basic Use

---
+++ SVG-Animation/script.js
@@ -0,0 +1,23 @@
+let initalPath = `M 10 100 Q 710 100 1420 100`;
+let finalPath = `M 10 100 Q 710 100 1420 100`;
+
+let string = document.querySelector(".string")
+
+
+string.addEventListener("mousemove", (dets) => {
+  initalPath = `M 10 100 Q ${dets.x} ${dets.y} 1420 100`
+  gsap.to("svg path", {
+    attr: { d: initalPath },
+    duration: 0.3,
+    ease: "power1.out"
+  })
+  console.log("inital path ", initalPath);
+})
+
+string.addEventListener("mouseleave", () => {
+  gsap.to("svg path", {
+    attr: { d: finalPath },
+    duration: 1.4,
+    ease: "elastic.out(1,0.1)"
+  })
+}) 
